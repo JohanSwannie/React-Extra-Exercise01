5690b4760f10d9bdbe3d6c335aa4f2fa68d12cca
Johan Swan <jswannieman@gmail.com>
2024-04-16 09:10:01 +1200

    Commit Home JSX Component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+function Home() {
+  return (
+    <div className="relative top-24">
+      <h2 className="text-center text-3xl mb-8">Welcome to the Home Page</h2>
+      <div>
+        <img
+          src="https://t4.ftcdn.net/jpg/06/82/38/47/360_F_682384717_SteEiKnERrg2JpdQ3m6o8WrNNEaE3BO2.jpg"
+          alt="birdies"
+          className="m-auto"
+          width="1000"
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Home;
