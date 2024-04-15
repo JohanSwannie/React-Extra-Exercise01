72f91e545dda5a5b9656e674c76c522e712ee56a
Johan Swan <jswannieman@gmail.com>
2024-04-16 09:11:26 +1200

    Commit inputData JS Component

---
+++ src/data/inputData.js
@@ -0,0 +1,72 @@
+export const data = [
+  {
+    name: "John Brown",
+    age: 57,
+    home: "Auckland",
+  },
+  {
+    name: "Noel Wittstock",
+    age: 81,
+    home: "Queenstown",
+  },
+  {
+    name: "Darren Brookfield",
+    age: 34,
+    home: "Nelson",
+  },
+  {
+    name: "Richard Peacock",
+    age: 41,
+    home: "Palmerston North",
+  },
+  {
+    name: "Luke Black",
+    age: 50,
+    home: "Wellington",
+  },
+  {
+    name: "Sarah Thomas",
+    age: 52,
+    home: "Tauranga",
+  },
+  {
+    name: "Jack Cushnie",
+    age: 39,
+    home: "Thames",
+  },
+  {
+    name: "Mary Cooper",
+    age: 46,
+    home: "Hamilton",
+  },
+  {
+    name: "Barry Sidaway",
+    age: 27,
+    home: "Christchurch",
+  },
+  {
+    name: "Annette Blink",
+    age: 40,
+    home: "Picton",
+  },
+  {
+    name: "Jilian Hoggard",
+    age: 55,
+    home: "Invercargill",
+  },
+  {
+    name: "Angus Beef",
+    age: 58,
+    home: "Taranaki",
+  },
+  {
+    name: "George Plimmer",
+    age: 47,
+    home: "Taupo",
+  },
+  {
+    name: "Natalie Green",
+    age: 30,
+    home: "Whitianga",
+  },
+];
